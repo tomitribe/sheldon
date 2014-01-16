2b00fd8f77d41cd7a858dd501e31bc94becc4644
dblevins <david.blevins@gmail.com>
2014-01-16 13:18:57 -0800

    Still rough sketch of what a real-world connector would look like

---
+++ src/main/java/org/tomitribe/telnet/adapter/TelnetActivationSpec.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomitribe.telnet.adapter;
+
+import org.tomitribe.crest.api.Command;
+import org.tomitribe.telnet.impl.Cmd;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ActivationSpec;
+import javax.resource.spi.InvalidPropertyException;
+import javax.resource.spi.ResourceAdapter;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TelnetActivationSpec implements ActivationSpec {
+
+    private ResourceAdapter resourceAdapter;
+    private final List<Cmd> cmds = new ArrayList<Cmd>();
+    private String prompt;
+    private Class beanClass;
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public List<Cmd> getCmds() {
+        return cmds;
+    }
+
+    @Override
+    public void validate() throws InvalidPropertyException {
+
+        // Get Commands
+        final Method[] methods = beanClass.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Command.class)) {
+                final Command command = method.getAnnotation(Command.class);
+                cmds.add(new Cmd(command.value(), method));
+            }
+        }
+
+        // Validate
+        if (this.prompt == null || this.prompt.length() == 0) {
+            this.prompt = "prompt>";
+        }
+        if (this.cmds.size() == 0) {
+            throw new InvalidPropertyException("No @Command methods");
+        }
+    }
+
+    @Override
+    public ResourceAdapter getResourceAdapter() {
+        return resourceAdapter;
+    }
+
+    @Override
+    public void setResourceAdapter(ResourceAdapter ra) throws ResourceException {
+        this.resourceAdapter = ra;
+    }
+}
---
+++ src/main/java/org/tomitribe/telnet/adapter/TelnetResourceAdapter.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomitribe.telnet.adapter;
+
+import org.tomitribe.telnet.api.TelnetListener;
+import org.tomitribe.telnet.impl.TelnetServer;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ActivationSpec;
+import javax.resource.spi.BootstrapContext;
+import javax.resource.spi.ConfigProperty;
+import javax.resource.spi.Connector;
+import javax.resource.spi.ResourceAdapterInternalException;
+import javax.resource.spi.endpoint.MessageEndpoint;
+import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.transaction.xa.XAResource;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Connector(
+    description = "Telnet ResourceAdapter",
+    displayName = "Telnet ResourceAdapter",
+    eisType = "Telnet Adapter",
+    version = "1.0"
+)
+public class TelnetResourceAdapter implements javax.resource.spi.ResourceAdapter {
+
+    private final Map<Integer, TelnetServer> activated = new HashMap<Integer, TelnetServer>();
+
+    /**
+     * Corresponds to the ra.xml <config-property>
+     */
+    @Size(min = 1, max = 0xFFFF)
+    @ConfigProperty(defaultValue = "2020")
+    @NotNull
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void start(BootstrapContext bootstrapContext) throws ResourceAdapterInternalException {
+    }
+
+    public void stop() {
+    }
+
+    public void endpointActivation(MessageEndpointFactory messageEndpointFactory, ActivationSpec activationSpec) throws ResourceException {
+        final TelnetActivationSpec telnetActivationSpec = (TelnetActivationSpec) activationSpec;
+
+        final MessageEndpoint messageEndpoint = messageEndpointFactory.createEndpoint(null);
+
+        // This messageEndpoint instance is also castable to the ejbClass of the MDB
+        final TelnetListener telnetListener = (TelnetListener) messageEndpoint;
+
+        final TelnetServer telnetServer = new TelnetServer(telnetActivationSpec, telnetListener, port);
+
+        try {
+            telnetServer.activate();
+            activated.put(port, telnetServer);
+        } catch (IOException e) {
+            throw new ResourceException(e);
+        }
+    }
+
+    public void endpointDeactivation(MessageEndpointFactory messageEndpointFactory, ActivationSpec activationSpec) {
+        final TelnetActivationSpec telnetActivationSpec = (TelnetActivationSpec) activationSpec;
+
+        final TelnetServer telnetServer = activated.remove(port);
+
+        try {
+            telnetServer.deactivate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final MessageEndpoint endpoint = (MessageEndpoint) telnetServer.getListener();
+
+        endpoint.release();
+    }
+
+    public XAResource[] getXAResources(ActivationSpec[] activationSpecs) throws ResourceException {
+        return new XAResource[0];
+    }
+
+}
