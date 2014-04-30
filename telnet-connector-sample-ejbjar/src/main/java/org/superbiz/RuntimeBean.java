5eac058429aea6efe6b23ff5aaf8178075b5b3aa
Jonathan Gallimore <jgallimore@tomitribe.com>
2014-04-30 01:12:19 +0100

    Refactoring to multi-module Maven project

--- telnet-connector-sample-ejbjar/src/main/java/org/superbiz/RuntimeBean.java
+++ telnet-connector-sample-ejbjar/src/main/java/org/superbiz/RuntimeBean.java
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.superbiz;
 
@@ -21,7 +21,7 @@ import org.tomitribe.crest.api.Default;
 import org.tomitribe.crest.api.Option;
 import org.tomitribe.telnet.api.TelnetListener;
 import org.tomitribe.util.Size;
-import org.tomitribe.util.*;
+import org.tomitribe.util.SizeUnit;
 
 import javax.ejb.MessageDriven;
 
--- telnet-connector-sample-ejbjar/src/main/java/org/superbiz/SystemPropertiesBean.java
+++ telnet-connector-sample-ejbjar/src/main/java/org/superbiz/SystemPropertiesBean.java
@@ -8,24 +8,22 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.superbiz;
+
+import java.io.File;
+
+import javax.ejb.MessageDriven;
 
 import org.tomitribe.crest.api.Command;
 import org.tomitribe.crest.val.Directory;
 import org.tomitribe.crest.val.Exists;
 import org.tomitribe.telnet.api.TelnetListener;
-import org.tomitribe.util.PrintString;
-
-import javax.ejb.MessageDriven;
-import java.io.File;
-import java.util.Map;
-import java.util.TreeSet;
 
 @MessageDriven
 public class SystemPropertiesBean implements TelnetListener {
